7f2163c393a1d68ac8eb360a424a47037e872b82
Varchaswa <varchaswacodes@gmail.com>
2023-10-11 08:58:05 +0530

    vite config updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Load environment variables from .env file
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'import.meta.env.VITE_URL': JSON.stringify(process.env.VITE_URL),
+    'import.meta.env.VITE_IF': JSON.stringify(process.env.VITE_IF),
+  },
+});
